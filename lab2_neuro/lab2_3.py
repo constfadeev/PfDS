04845f6430f6f9fe593818756b290e23bd18e45a
Const <sportul@yandex.ru>
2020-07-01 14:12:45 +0500

    add lab 2.3

--- lab2_neuro/lab2_3.py
+++ lab2_neuro/lab2_3.py
@@ -1,17 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import ndimage
 import glob
 from PIL import Image
 
 '''переводит картинку в одномерный массив''' 
-def image_to_array(path): 
+def image_to_array(path, rotate=0): 
     img = Image.open(path)
-    arr = np.array(img)
+    img_rot = ndimage.rotate(img, rotate, reshape=False)
+    arr = np.array(img_rot)
     a = np.zeros(0)
     for i in arr:
         a = np.concatenate((a, np.transpose(i)[-1]), axis=None)
-    return np.where(a==255., 1, a)
+    return np.where(a>=128., 1, 0)
 
 '''создает тренировочный датасет с выходом матрицы значений X и соответсвующего значения y'''
 def train_ds(DIR_1):
@@ -21,8 +21,15 @@ def train_ds(DIR_1):
     for file in files:
         all_png = np.vstack((all_png, image_to_array(file)))
         val = np.vstack((val, [int(x) for x in list(file[-7:-4])]))
-    print(all_png[1:], val[1:])
+    #print(all_png[1:], val[1:])
     return all_png[1:], val[1:]
+
+def test_to_rotate(DIR_2, rotate=0):
+    files = glob.glob(f"{DIR_2}\*.png")
+    all_png = np.zeros(25)
+    for file in files:
+        all_png = np.vstack((all_png, image_to_array(file, rotate)))
+    return all_png[1:]
 
 DIR = r'.\train_8' #расположение файлов для обучения
 
@@ -49,7 +56,19 @@ def train():
     return (w != _w).any()
            
 while train():
-    print(w)
+    #print(w)
+    pass
 
-for i in range(8):
-    print(f(D[i]))
+#Вращение картинки
+eq_sum = 0
+for i in range(0,100,10):
+    eq = 0
+    D_rot = test_to_rotate(DIR, rotate=i)
+    for j in range(8):
+        if np.array_equal(f(D[j]), f(D_rot[j])): #если получившиеся значения равны, плюсуем
+            eq += 1
+    print(f'При повороте на {i} градусов привильно угадано {eq} чисел, {eq/8*100}%')
+    eq_sum += eq
+
+print(f'Всего правильно угаданных значений при повороте картинки: {eq_sum}')
+print(f'Процент правильно угаданных значений: {eq_sum/10/8*100}')
